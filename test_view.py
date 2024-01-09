d22ab3cc8c2aa7591358c9bfbacdcfae13c25e9a
Michael Ricks-Aherne <mike@cognitivespace.com>
2024-01-09 17:11:45 -0600

    faster tests, fixed import

--- test_view.py
+++ test_view.py
@@ -1,16 +1,17 @@
 import helper
+import items
 import entities
 
 def test_view_piles_on_all_cards(monkeypatch):
-  all_cards = [card for card in entities.cards.values()]
+  all_cards = [card for card in items.cards.values()]
   entity = entities.create_player()
   entity.energy = 3
   all_conditions = ['Upgraded' ,'Upgradable' ,'Removable' ,'Skill' ,'Attack' ,'Power' ,'Playable']
 
   with monkeypatch.context() as m:
     # Patch sleeps so it's faster
-    # m.setattr(helper, 'sleep', lambda x: None)
-    # m.setattr(entities, 'sleep', lambda x: None)
+    m.setattr(helper, 'sleep', lambda x: None)
+    m.setattr(entities, 'sleep', lambda x: None)
 
     for condition in all_conditions:
       print(f"===== Condition: {condition} ======")
