25c6a4ac9a874067f9b113715e2a489a091427aa
Michael Ricks-Aherne <mike@cognitivespace.com>
2024-01-11 21:03:13 -0600

    TEST AT WARP SPEED

--- test_player.py
+++ test_player.py
@@ -35,9 +35,9 @@ def test_all_attack_cards_with_all_relics(monkeypatch):
     # Patch some side effects
     with monkeypatch.context() as m:
         m.setattr('builtins.input', patched_input)
-        # m.setattr(game, 'sleep', lambda x: None)
-        # m.setattr(helper, 'sleep', lambda x: None)
-        # m.setattr(entities, 'sleep', lambda x: None)
+        m.setattr(helper, 'sleep', lambda x: None)
+        m.setattr(entities, 'sleep', lambda x: None)
+        m.setattr(items, 'sleep', lambda x: None)
         helper.view.clear = replacement_clear_screen
 
         # Let 'er rip!
